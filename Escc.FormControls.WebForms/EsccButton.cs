31afa3e8aa66f132e289a79de45296cf5735071f
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-11-10 13:48:28 +0000

    Support adding attributes other than class to an EsccButton

--- Escc.FormControls.WebForms/EsccButton.cs
+++ Escc.FormControls.WebForms/EsccButton.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Escc.FormControls.WebForms
 {
     /// <summary>
@@ -6,22 +8,18 @@ namespace Escc.FormControls.WebForms
     public class EsccButton : System.Web.UI.WebControls.Button
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="EsccButton"/> class.
-        /// </summary>
-        public EsccButton()
-            : base()
-        {
-        }
-
-        /// <summary>
         /// Write out an &lt;input&gt; element which uses only valid attributes
         /// </summary>
         /// <param name="writer"></param>
         public override void RenderBeginTag(System.Web.UI.HtmlTextWriter writer)
         {
             writer.Write("<input type=\"submit\" value=\"{0}\" name=\"{1}\" id=\"{2}\"", this.Text, this.UniqueID, this.ClientID);
-            if (this.CssClass != null && this.CssClass.Length > 0) writer.Write(" class=\"{0}\"", this.CssClass);
+            if (!string.IsNullOrEmpty(this.CssClass)) writer.Write(" class=\"{0}\"", this.CssClass);
             if (this.Attributes["disabled"] != null) writer.Write(" disabled=\"{0}\"", this.Attributes["disabled"]);
+            foreach (string attribute in this.Attributes.Keys)
+            {
+                if (attribute.StartsWith("data-", StringComparison.OrdinalIgnoreCase)) writer.Write($" {attribute}=\"{this.Attributes[attribute]}\"");
+            }
             writer.Write(" />");
         }
 
